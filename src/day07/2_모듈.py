7b8bdd8ce99ed05e5616934f32c43aeafb7e8e44
Kimviruscool <sinsa9122@naver.com>
2024-08-22 11:16:35 +0900

    2024 - 08 - 22

---
+++ src/day07/2_모듈.py
@@ -0,0 +1,29 @@
+# 2_모듈.py
+
+import mod1
+#[1] #import 모듈이름 :
+    #모듈이름 .함수명()
+
+mod1.add(3,4)
+
+#[2] from 모듈이름 import 함수명 , g함수명
+from mod1 import sub
+sub(3,4)
+
+#[3] from 모듈이름 import *
+from mod1 import *
+add(3,4)
+sub(4,1)
+
+#[4]
+import mod2
+print(mod2.PI) #3.141592
+
+a = mod2.Math
+print(a) #<class 'mod2.Math'>
+print (a.solv(2) ) #12.566368
+print(mod2.add(3,4))
+
+from mod2 import Math, PI
+print(PI)
+print(Math)
---
+++ src/day07/mod2.py
@@ -0,0 +1,12 @@
+# mod2.py
+
+PI = 3.141592 #변수
+#클래스
+class Math :
+    #메소드
+    def solv(self, r):
+        return PI * (r ** 2)
+
+    #함수
+    def add( a, b):
+        return a + b
