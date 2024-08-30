194f429e98d4172d691a2881f6f1fa1dcd9c4df7
Kimviruscool <sinsa9122@naver.com>
2024-08-30 09:29:46 +0900

    2024 - 08 - 30

--- src/day12/6_부동산기술통계/service.py
+++ src/day12/6_부동산기술통계/service.py
@@ -1,9 +1,7 @@
 # 6_부동산기술통계.py
 # 부동산 실거래가 :
 # 인천광역시 부평구 전월세 1년치 csv 수집
-# csv 파일을 판다스의 데이터프레임 으로 변경
-# 데이터 탐색(기술 통계)
-    # 기술통계 탐색 결과를 SPRING INDEX6.HTML 테이블 형식으로 출력 (HTTP 매핑 임의로 지정)
+
 # 데이터 모델링 (그룹화)
     #전월세 기준으로 그룹해서 전용면적의 통계
 # 추가
@@ -12,10 +10,21 @@
 
 import json
 import pandas as pd
-import matplotlib.pyplot as plt
-def readapt(fileName):
-    df = pd.read_csv(f'{fileName}.csv', encoding='utf-8', engine='python')
-    # print(df)
-    jsonResult = df.to_json(orient='records', force_ascii=False)
-    result = json.loads(jsonResult)
-    return result
+from flask import Flask
+
+# csv 파일을 판다스의 데이터프레임 으로 변경
+
+#1. 데이터 read_csv 를 사용하여 csv파일 읽어오기
+data = pd.read_csv('부평아파트(전월세)_실거래가.csv', encoding='utf-8', engine='python')
+#2. 불러온 데이터의 이름을 재정의
+data.rename(columns={'월세금(만원)':'월세금만원'} , inplace=True)
+data.rename(columns={'전용면적(㎡)':'전용면적'} , inplace=True)
+# print(data) #확인용
+
+#함수 생성
+def totaldata() :
+    total = data.describe() #통계 #describe()
+
+# 데이터 탐색(기술 통계)
+    # 기술통계 탐색 결과를 SPRING INDEX6.HTML 테이블 형식으로 출력 (HTTP 매핑 임의로 지정)
+
