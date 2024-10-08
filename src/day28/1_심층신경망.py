8c7542653f8cdc29b1019137b8684bd74b2f05e7
Kimviruscool <sinsa9122@naver.com>
2024-10-08 12:20:17 +0900

    2024 - 10 - 08

--- src/day28/1_심층신경망.py
+++ src/day28/1_심층신경망.py
@@ -1,6 +1,7 @@
 # 1_심층신경망.py  p.073
 
 import tensorflow as tf
+from tensorflow.python.keras.saving.saved_model.serialized_attributes import metrics
 
 # 케라스의 내장된 데이터셋에서 mnist(손글씨 이미지) 데이터셋 로드
 mnist = tf.keras.datasets.mnist
@@ -77,3 +78,51 @@ model = tf.keras.Sequential([
 # 각 레이어(은닉층) 개수 , 각 노드의 개수는 중요한 하이퍼 파라미터가 된다.
 
 print(model.summary() )
+
+# 손실함수
+
+#(1) 이진 분류 : 출력 노드 1개 , sigmoid 일경우
+model.compile(loss = 'binary_crossentropy')
+
+#(2) y가 원핫 벡터 인 경우
+    # y = 5 일때 원핫 = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+model.compile(loss = 'categorical_crossentroypy')
+
+#(3) y가 원핫 벡터 가 아닐 때
+    # y = 5
+model.compile(loss='sparse_categorical_crossentropy')
+
+# 옵티마이저
+#(1) 클래스로 지정하는 방법
+# adam = tf.keras.optimizers.Adam(lr = 0.001)
+#ValueError: Argument(s) not recognized: {'lr': 0.001}
+adam = tf.keras.optimizers.Adam(learning_rate = 0.001) #텐서플로2 부터는 lr 대신 > learning_rate 사용한다.
+
+model.compile(optimizer = adam)
+#(2) 문자열로 지정하는 방법
+model.compile(optimizer = 'adam')
+
+# 평가 지표 : ACCURACY(acc) : 1에 가까울수록 성능이 좋다.
+# (1) 클래스로 지정하는 방법
+acc = tf.keras.metrics.SparseCategoricalAccuracy()
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=[acc])
+
+# (2) 문자열로 지정
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# 훈련
+# 훈련모델.fit(훈련독립변수 , 훈련종속변수 , epochs = 학습반복횟수, validetion_data = (테스트독립변수, 테스트종속변수) )
+model.fit(x_train, y_train, epochs = 10, validation_data = (x_test,y_test))
+'''
+1875/1875 ━━━━━━━━━━━━━━━━━━━━ 4s 2ms/step - accuracy: 0.9945 - loss: 0.0160 - val_accuracy: 0.9800 - val_loss: 0.0872
+    - 1. Epoch 10/10 (현재 훈련중인 반복훈련(에포크)수 )
+    - 2. 35/1875 - 현재 진행중인 배치의 번호
+        총 = 1875, 총 데이터수가 60000, 총 배치수 : 32개 > 총데이터수/총배치수
+    - 배치수 : 모델 훈련에서 전체를 구분한 집합 수 #주로 32개 64개 128개 사용 # 기본값은 32개 사용한다.
+'''
+
+#평가
